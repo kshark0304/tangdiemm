9359ed8cc681c4ea543f62ab97794aa58af76216
kshark0304 <94666705+kshark0304@users.noreply.github.com>
2021-12-26 18:53:19 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle : 'Diễm à!' ,
+    introDesc : Thường lệ vốn trái đất
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',.
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Làm người iêu khánh nha <3',
+    reply: 'Diễm Yêu Khánh <33333333',
+    mess: 'Anh biết mà 🥰. Yêu diễm nhiều nhiều 😘😘',
+    messDesc: 'Đi học Mua tà tữa Cho:33.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
